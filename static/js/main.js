b1f8125afbb141b8db5dd43764e1977f6b62ba24
HKJL10201 <HKJL10201@gmail.com>
2025-02-21 02:43:19 -0700

    update

--- static/js/main.js
+++ static/js/main.js
@@ -8,74 +8,22 @@ window.onload = function () {
 
   function rightSlide() {
     // img right shift
-    var inb = "header-img-dot" + index.toString(); // bottom buttons, start with "header-img-dot"，append `index`
     index = (index + 1) % 5; //index+1
-    var ind = "header-img-dot" + index.toString(); // get id of next button
-    document.getElementById(inb).style.backgroundColor = "rgba(255,255,255,.9)"; // last button turns white
-    document.getElementById(ind).style.backgroundColor = "rgba(180,180,180,.9)"; // curr button turns gray
     $("#header-imgs").animate({ marginLeft: ml[index] }); // set the `margin-left` of img frame to the next item of `ml` array
   }
   function leftSlide() {
     // img left shift
-    var inb = "header-img-dot" + index.toString();
     index = (index + 4) % 5;
-    var ind = "header-img-dot" + index.toString();
-    document.getElementById(inb).style.backgroundColor = "rgba(255,255,255,.9)";
-    document.getElementById(ind).style.backgroundColor = "rgba(180,180,180,.9)";
     $("#header-imgs").animate({ marginLeft: ml[index] }); // set the `margin-left` of img frame to the last item of `ml` array
-  }
-  function lostLastDot() {
-    var inb = "header-img-dot" + index.toString();
-    document.getElementById(inb).style.backgroundColor = "rgba(255,255,255,.9)";
   }
 
   $(document).ready(function () {
-    // $("#rig-btn").click(function () {
-    //   rightSlide();
-    //   clearInterval(timer);
-    // });
-    // $("#lft-btn").click(function () {
-    //   leftSlide();
-    //   clearInterval(timer);
-    // });
-    $("#header-img-dot0").click(function () {
-      lostLastDot();
-      index = 0;
-      document.getElementById("header-img-dot0").style.backgroundColor =
-        "rgba(180,180,180,.9)";
-      $("#header-imgs").animate({ marginLeft: "0%" });
+    $("#header-img-right-button").click(function () {
+      rightSlide();
       clearInterval(timer);
     });
-    $("#header-img-dot1").click(function () {
-      lostLastDot();
-      index = 1;
-      document.getElementById("header-img-dot1").style.backgroundColor =
-        "rgba(180,180,180,.9)";
-      $("#header-imgs").animate({ marginLeft: "-100%" });
-      clearInterval(timer);
-    });
-    $("#header-img-dot2").click(function () {
-      lostLastDot();
-      index = 2;
-      document.getElementById("header-img-dot2").style.backgroundColor =
-        "rgba(180,180,180,.9)";
-      $("#header-imgs").animate({ marginLeft: "-200%" });
-      clearInterval(timer);
-    });
-    $("#header-img-dot3").click(function () {
-      lostLastDot();
-      index = 3;
-      document.getElementById("header-img-dot3").style.backgroundColor =
-        "rgba(180,180,180,.9)";
-      $("#header-imgs").animate({ marginLeft: "-300%" });
-      clearInterval(timer);
-    });
-    $("#header-img-dot4").click(function () {
-      lostLastDot();
-      index = 4;
-      document.getElementById("header-img-dot4").style.backgroundColor =
-        "rgba(180,180,180,.9)";
-      $("#header-imgs").animate({ marginLeft: "-400%" });
+    $("#header-img-left-button").click(function () {
+      leftSlide();
       clearInterval(timer);
     });
   });
@@ -93,35 +41,9 @@ window.onload = function () {
   container.onmouseout = play; // start shifting if mouse leave
 
   //play();
-  $("#site-left").fadeOut(0);
-  $("#fd-left").fadeOut(0);
   $("#Top-button").fadeOut(0);
   document.getElementById("Top-button").style.height = "50px";
 };
-
-// gallery
-// $(document).ready(function () {
-//   $("#site-right").click(function () {
-//     $("#sites").animate({ marginLeft: "-732px" });
-//     $("#site-right").fadeOut(100);
-//     $("#site-left").fadeIn(100);
-//   });
-//   $("#site-left").click(function () {
-//     $("#sites").animate({ marginLeft: "0px" });
-//     $("#site-left").fadeOut(100);
-//     $("#site-right").fadeIn(100);
-//   });
-//   $("#fd-right").click(function () {
-//     $("#food").animate({ marginLeft: "-520px" });
-//     $("#fd-right").fadeOut(100);
-//     $("#fd-left").fadeIn(100);
-//   });
-//   $("#fd-left").click(function () {
-//     $("#food").animate({ marginLeft: "0px" });
-//     $("#fd-left").fadeOut(100);
-//     $("#fd-right").fadeIn(100);
-//   });
-// });
 
 // Top button and navigator
 $(function () {
